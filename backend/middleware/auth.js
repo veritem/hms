bc6e73d4c0bbb0beeed8a417b4432db79e3326f1
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-11-07 18:38:02 +0200

    added auth-middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,45 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from './async'
+import ErrorResponse from './error'
+import User from '../models/User'
+
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // Set token from Bearer token in header
+    token = req.headers.authorization.split(' ')[1]
+  }
+
+  if (!token) {
+    return next(new ErrorResponse('Not authorized to access this route', 401))
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SCRET)
+
+    req.user = await User.findById(decoded._id)
+
+    next()
+  } catch (err) {
+    return next(new ErrorResponse('Not authorized to access this route', 401))
+  }
+})
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      )
+    }
+    next()
+  }
+}
