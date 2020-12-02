cc6759cab2c7c4c5c6008d069e5ae751880a2b43
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-02 18:48:28 +0200

    items model updated to v3.0

--- routes/items.js
+++ routes/items.js
@@ -9,11 +9,31 @@ import {
 
 const router = express.Router({ mergeParams: true })
 
+// /**
+//  * @swagger
+//  * components:
+//  *   schemas:
+//  *   Items:
+//  *     properties:
+//  *       itemName:
+//  *         type: string
+//  *       supplier_id:
+//  *         type: string
+//  *       itemType:
+//  *         type: string
+//  *       item_details:
+//  *         type: string
+//  *       status:
+//  *         type: string
+//  *         enum: [ACTIVE,PENDING,INACTIVE]
+//  */
+
 /**
  * @swagger
- * definitions:
- *   Items:
- *     properties:
+ * components:
+ *   schemas:
+ *     Items:
+ *      properties:
  *       itemName:
  *         type: string
  *       supplier_id:
