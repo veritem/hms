58afd8a1c032e154b3c8e7b519305786e7f52346
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-06 12:56:46 +0200

    added room types

---
+++ models/Room.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose'
+
+const RoomSchema = new Schema({
+  type: {
+    enum: ['single', 'double', 'triple', 'meeting'],
+  },
+  status: {
+    enum: ['ACTIVE', 'BOOKED', 'IDLE'],
+    default: 'IDLE',
+  },
+  number: {
+    type: String,
+  },
+  room_description: {
+    type: String,
+  },
+})
+
+module.exports = mongoose.model('Room', RoomSchema)
