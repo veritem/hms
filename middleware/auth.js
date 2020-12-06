c18638575fbb0c31444ca2aebb62bb020aa6e83c
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-12-06 12:19:05 +0200

    getMe api done

--- middleware/auth.js
+++ middleware/auth.js
@@ -23,8 +23,8 @@ exports.protect = asyncHandler(async (req, res, next) => {
   try {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-    req.user = await User.findById(decoded._id)
+    console.log(decoded)
+    req.user = await User.findById(decoded.id)
 
     next()
   } catch (err) {
