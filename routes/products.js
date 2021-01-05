7e082dcfed5453133f785904186f1ab221d630d6
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-05 22:23:04 +0200

    added get product route

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,7 @@
 import express from 'express'
+import advancedResults from '../middleware/advancedResults'
 const router = express.Router()
+import { getProducts } from '../controllers/product'
 
 /**
  * @swagger
@@ -16,5 +18,7 @@ const router = express.Router()
  *       inStock:
  *         type: number
  */
+
+router.route('/').get(advancedResults('Product'), getProducts)
 
 module.exports = router
