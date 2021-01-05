f60c4a3f23ceb845bbcc23069c66e89957cbf4cb
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-05 22:27:34 +0200

    documented the api  i have done

--- routes/products.js
+++ routes/products.js
@@ -20,6 +20,17 @@ import Product from '../models/Product'
  *         type: number
  */
 
+/**
+ * @swagger
+ * /api/v1/products:
+ *  get:
+ *    tags:
+ *    - [Products]
+ *    description: Use to request all products
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.route('/').get(advancedResults(Product), getProducts)
 
 module.exports = router
