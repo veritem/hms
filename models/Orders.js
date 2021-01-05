c51ddbb7366c83d26a212561bcd6980b8a468ac8
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-05 22:07:31 +0200

    orders model done

--- models/Orders.js
+++ models/Orders.js
@@ -5,15 +5,31 @@ const OrderSchema = new Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'User',
   },
+  order_details: [
+    {
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      amount: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
   created_at: {
     type: Date,
     default: Date.now(),
   },
-  order_details: [
-    {
-      product: {},
-    },
-  ],
 })
 
 module.exports = mongoose.model('Order', OrderSchema)
