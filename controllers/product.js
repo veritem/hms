5dfd3417a41fe13be2cc0ee036329437e1785340
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-01-05 22:20:44 +0200

    get products added advanced result options

--- controllers/product.js
+++ controllers/product.js
@@ -1,1 +1,5 @@
-export default function getProducts() {}
+import asyncHandler from '../middleware/async'
+
+exports.getProducts = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults)
+})
